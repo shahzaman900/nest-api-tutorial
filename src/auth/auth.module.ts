bd892f84f2d6053d8bebf02bab5f9599501fe4ee
sheezu900 <shahzamanlive2@gmail.com>
2023-10-23 20:52:29 +0500

    setup complete

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Module } from '@nestjs/common'
+
+@Module({})
+export class AuthModule {
+}
+
